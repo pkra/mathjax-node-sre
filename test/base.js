29df3d4058ab55a5dbe7b5414bb360a44a076b09
Peter Krautzberger <p.krautzberger@gmail.com>
2016-11-15 23:15:06 +0100

    second commit

--- test/base.js
+++ test/base.js
@@ -4,8 +4,7 @@ var mjsre = require('../lib/main.js').typeset;
 tape('Base check', function(t) {
   t.plan(1);
   var input = 'x';
-  mjsre({math: input, format: ["TeX"],svg: true},function(result){
+  mjsre({math: input, format: "TeX", svg: true},function(result){
     t.ok(result.svg, 'basic test');
   });
-
 });
